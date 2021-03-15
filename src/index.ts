1d945bcb366a9c8fbb4d31f3ada318ee4fc320e6
Joselie Castaneda <jcing.jc@gmail.com>
2021-03-15 20:55:06 +0800

    hehe

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,8 @@ function addTwoNumbers(a: number, b: number): number {
   return a + b;
 }
 
+function addTwoNumberzzzz(a: number, b: number): number {
+  return a + b;
+}
+
 console.log(addTwoNumbers(100, 20222));
